8567790ce583309729de226e8650e75250272f99
0b1 <6540741+0bOne@users.noreply.github.com>
2021-12-18 13:15:07 -0500

    added timings and output formats

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const OptionsLoader = require("./lib/OptionsLoader");
 const Logger = require("./lib/Logger");
 const UserGuide = require("./lib/UserGuide");
 const OutputFormatter = require("./lib/OutputFormatter");
-
+const StopWatch = require("./lib/StopWatch");
 
 const options = OptionsLoader.Load();
 
@@ -13,22 +13,18 @@ const logger = new Logger(options);
 
 const jc = new Loci(logger);
 const startTime = new Date();
+const sw = new StopWatch();
+
 const output = jc.Begin(options);
 
 logger.Debug("options", options);
 logger.Info(output);
 
-const endTime = new Date();
-const difference = endTime.getTime() - startTime.getTime();
-const seconds = Math.round(difference / 10) / 100;
-logger.Info("time taken: " + seconds + " s");
 
 const of = new OutputFormatter(options);
 const result = of.Format(output);
+console.log("");
+//console.log(output);
+
 console.log(result);
-
-
-
-
-
 
